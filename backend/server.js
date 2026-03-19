dc361ef9349d18338f15cd5e5edce5f5017c98d5
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-03-19 20:04:06 +0530

    auth middleware,authroutes, and authcontrollers setup complete

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -25,7 +26,7 @@ connectDB();
 app.use(express.json());
 
 //Routes
-app.use("/api/auth", authRoutes);
+app.use("/api/auth",authRoutes);
 // app.use("/api/sessions", sessionRoutes);
 // app.use("/api/questions", questionRoutes);
 
