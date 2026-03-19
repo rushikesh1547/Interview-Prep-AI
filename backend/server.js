3457fec2be007f1dd7077f6372a3ff60ea841c3e
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-03-19 19:21:46 +0530

    Auth routes setup done

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,12 @@ connectDB();
 app.use(express.json());
 
 //Routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 //Server upload folder
 
