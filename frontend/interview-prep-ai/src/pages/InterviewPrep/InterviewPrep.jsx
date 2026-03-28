e0d508f31b15db7ccd0c1b2838072084998ba3f0
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-03-29 00:32:13 +0530

    Clean project without .env

---
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import moment from 'moment';
+import {AnimatePresence, motion} from "framer-motion";
+import { LuCircleAlert, LuListCollapse  } from 'react-icons/lu';
+import SpinnerLoader from '../../components/Loader/SpinnerLoading';
+import { toast } from 'react-hot-toast';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import RoleInfoHeader from './components/RoleInfoHeader';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import QuestionCard from '../../components/Cards/QuestionCard';
+
+const InterviewPrep = () => {
+
+  const { sessionId } = useParams();
+
+  const [sessionData, setSessionData] = useState(null);
+  const [errorMsg, setErrorMsg] =useState("");
+
+  const [openLearnMoreDrawer, setOpenLearnMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setIsUpdateLoader] = useState(false);
+
+  //Fetch session data by sessionId
+  const fetchSessionDetailsById = async () => {
+      try{
+        const response = await axiosInstance.get(
+          API_PATHS.SESSION.GET_ONE(sessionId)
+        );
+
+        if(response.data && response.data.session){
+          setSessionData(response.data.session);
+        }
+      }catch(error){
+        console.error("Error:", error);
+      }
+  };
+
+  //Generate Concept explanation
+  const generateConceptExplanation = async (question) => {};
+
+  //Pin Question 
+  const toggleQuestionPinStatus = async () => {};
+
+  //Add more questions to a session
+  const uploadMoreQuestions = async () => {};
+
+  useEffect(() => {
+    if(sessionId){
+      fetchSessionDetailsById();
+    }
+
+    return () => {};
+  },[]);
+
+  return (
+    <DashboardLayout>
+      <RoleInfoHeader
+      role= {sessionData?.role || ""}
+      topicsToFocus={sessionData?.topicsToFocus || ""}
+      experience={sessionData?.experience || "-"}
+      questions={sessionData?.questions?.length || "-"}
+      description={sessionData?.description || ""}
+      lastUpdated={
+        sessionData?.updatedAt 
+        ? moment(sessionData.updatedAt).format("DD MM YYYY") 
+        : ""}
+       />
+
+        <div className='container mx-auto pt-4 pb-4 px-4 md:px-0'>
+          <h2 className='text-lg font-semibold color-black'>
+            Interview Q & A
+          </h2>
+
+          <div className='grid grid-cols-12 gap-4 mt-5 mb-10'>
+            <div
+            className={`col-span-12 ${
+              openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+            }`}>
+
+              <AnimatePresence>
+                {sessionData?.questions?.map((data, index) => {
+                  return (
+                    <motion.div
+                    key={data.id || index}
+                    initial={{ opacity: 0 , y: 20}}
+                    animate={{ opacity: 1 , y: 0}}
+                    exit={{ opacity: 0 ,scale: 0.95}}
+                    transition={{ 
+                      duration: 0.3 ,
+                      type: "spring",
+                      stiffness: 100,
+                      delay: index * 0.1,
+                      damping: 15,
+
+                    }}
+                    layout //This is the key prop that annimates position changes
+                    layoutId={`question-${data._id || index}`} //helps framer track specific items
+                    >
+                      <>
+                      <QuestionCard
+                      question={data?.question}
+                      answer={data?.answer}
+                      onLearnMore ={() => generateConceptExplanation(data.question)}
+                      isPinned={data?.isPinned}
+                      onTogglePin={() => toggleQuestionPinStatus(data._id)}
+                      />
+                      </>
+                    </motion.div>
+                  );
+                })}
+              </AnimatePresence>
+            </div>
+            </div>
+            </div>
+
+    </DashboardLayout>
+  );
+};
+
+export default InterviewPrep;
---
+++ frontend/interview-prep-ai/src/utils/apiPaths.js
@@ -0,0 +1,30 @@
+export const BASE_URL = "http://localhost:8000";
+
+export const API_PATHS = {
+    AUTH: {
+        REGISTER: "/api/auth/register",//Signup
+        LOGIN: "/api/auth/login",//Authenticate user & return JWT token
+        GET_PROFILE: "/api/auth/profile",//Get logged-in user details
+    },
+
+    IMAGE: {
+        UPLOAD_IMAGE: "/api/auth/upload-image", //Upload user profile image
+    },
+
+    AI: {
+        GENERATE_QUESTIONS: "/api/ai/generate-questions",//Generate interview questions
+        GENERATE_EXPLANATIONS: "/api/ai/explanations",//Generate concept explanations
+    },
+    SESSION: {
+        CREATE: "/api/sessions/create",//Create new session
+        GET_ALL: "/api/sessions/my-sessions",//Get all user sessions
+        GET_ONE: (id) => `/api/sessions/${id}`,//Get session details with questions
+        DELETE: (id) => `/api/sessions/${id}`,//Delete session
+    },
+
+    QUESTION: {
+        ADD_TO_SESSION: "/api/questions/add",//Add additional questions to existing session
+        PIN: (id) => `/api/questions/${id}/pin`,//Pin or unpin a question
+        UPDATE_NOTE: (id) => `/api/questions/${id}/note`,//Add note to a question
+    },
+}
