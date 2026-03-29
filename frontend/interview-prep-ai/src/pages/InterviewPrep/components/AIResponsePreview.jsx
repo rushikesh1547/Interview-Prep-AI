25487a6e60f6f6f3b06b63b437fea7cac29c1048
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-03-29 18:51:07 +0530

    all the bugs resolved

--- frontend/interview-prep-ai/src/pages/InterviewPrep/components/AIResponsePreview.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/components/AIResponsePreview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {LuCopy, LuCheck, LuCode} from "react-icons/lu";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
