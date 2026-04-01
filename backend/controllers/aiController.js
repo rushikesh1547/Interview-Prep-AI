986686f8f293646a41a83a13aaf8667a9a16471a
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-01 19:34:20 +0530

    gemini model changed

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -28,7 +28,7 @@ const generateInterviewQuestions = async (req, res) => {
         );
 
         const response = await genAI.models.generateContent({
-            model: "gemini-1.5-flash",
+            model: "gemini-3.1-flash-lite-preview",
             contents: prompt,
         });
 
@@ -67,7 +67,7 @@ const generateConceptExplanation = async (req, res) => {
         const prompt = conceptExplainPrompt(question);
 
         const response = await genAI.models.generateContent({
-            model: "gemini-1.5-flash",
+            model: "gemini-3.1-flash-lite-preview",
             contents: prompt,
         });
 
